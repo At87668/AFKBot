78a5eac3e27fc4a47ab5f8a896d219658623fc03
Author87668 <105369287+At87668@users.noreply.github.com>
2023-08-14 21:30:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "AFKBot is initing..."
 sudo apt-get install -y dpkg
 sudo dpkg -i libicu55_55.1-7ubuntu0.5_amd64.deb
 chmod 777 MinecraftClient
+echo "AFKBot is started."
 nohup ./MinecraftClient AFKBot.ini
+echo "Client is crashed!"
